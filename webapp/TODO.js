b49fe044cc5de122f8b5afb2a94554166f779d1c
Nadav Leshem <nadavl@wix.com>
2014-09-02 13:44:45 +0300

    removed "todo" and added a new favicon

--- webapp/TODO.js
+++
@@ -1,8 +0,0 @@
-// [x] TODO when posting a message get the user from the session, or, if superuser, from the payload
-// [x] TODO add custom queries
-// [-] TODO extract logic from controllers and have it Unit tested
-// [x] TODO create a rest interface
-// [x] TODO add count to note after a vote
-// [ ] TODO add index for all shit
-// [x] TODO add search for messages
-// [ ] TODO add favicon
